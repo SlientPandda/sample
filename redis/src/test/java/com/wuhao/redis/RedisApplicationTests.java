877d3bda93283d06ee29e824694230a2c54ac1df
wuhao <2573086878@qq.com>
2023-07-07 16:02:36 +0800

    增加部分未版本控制的代码

---
+++ redis/src/test/java/com/wuhao/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuhao.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ tips/src/test/java/com/wuhao/tips/either/TestEitherTest.java
@@ -0,0 +1,10 @@
+package com.wuhao.tips.either;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ */
+class TestEitherTest {
+
+}
